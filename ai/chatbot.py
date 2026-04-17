6a7a7cdc581f4ce2e3dd1e07735ce865b22f7670
Diwakar Jha <116476030+Stfu-diwakar@users.noreply.github.com>
2026-04-18 03:27:24 +0530

    Update chatbot.py

--- ai/chatbot.py
+++ ai/chatbot.py
@@ -1,9 +1,10 @@
-import google.generativeai as genai
 import streamlit as st
+import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
+
 def ask_ai(user_query, context=""):
     prompt = f"""
     You are a smart assistant for a large sports stadium.
@@ -17,5 +18,13 @@ def ask_ai(user_query, context=""):
     Provide a short, clear, and helpful answer.
     """
 
-    response = model.generate_content(prompt)
-    return response.text
+    try:
+        response = model.generate_content(prompt)
+
+        if hasattr(response, "text") and response.text:
+            return response.text
+
+        return response.candidates[0].content.parts[0].text
+
+    except Exception as e:
+        return "Unable to generate response at the moment."
